72aac901c78db9af9902cd68a0072b26a6758a3a
yechong <240542223@qq.com>
2023-08-21 00:05:35 +0800

    init

--- test/flutter_wechat_login_method_channel_test.dart
+++ test/flutter_wechat_login_method_channel_test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_wechat_login/flutter_wechat_login_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelFlutterWechatLogin platform = MethodChannelFlutterWechatLogin();
   const MethodChannel channel = MethodChannel('flutter_wechat_login');
 
   tearDown(() {
